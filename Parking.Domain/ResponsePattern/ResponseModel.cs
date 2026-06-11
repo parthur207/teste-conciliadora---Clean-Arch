ca8fe1beeab0c826c2bdf1195cad056377f98fc9
Paulo Andrade <parthur207@gmail.com>
2026-06-11 00:03:11 -0300

    Criação dos padrões de resposta para consultas e comandos.

---
+++ Parking.Domain/ResponsePattern/ResponseModel.cs
@@ -0,0 +1,17 @@
+﻿using Parking.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking.Domain.ResponsePattern
+{
+    public class ResponseModel<T> 
+    {
+        public ResponseModel(){}
+        public T? Content { get; set; }
+        public string? Message { get; set; }
+        public ResponseStatusEnum Status { get; set; }
+    }
+}
---
+++ Parking.Domain/ResponsePattern/SimpleResponseModel.cs
@@ -0,0 +1,17 @@
+﻿using Parking.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking.Domain.ResponsePattern
+{
+    public class SimpleResponseModel
+    {
+        public SimpleResponseModel(){}
+
+        public ResponseStatusEnum Status { get; set; }
+        public string? Message { get; set; }
+    }
+}
