3ad751b09392ef5dc333b35b4dcc0e2b17915e16
Paulo Andrade <parthur207@gmail.com>
2026-06-11 00:12:15 -0300

    Configurações gerais para uso do swagger, permissoes de requisições da camada do front.

--- Parking.API/Program.cs
+++ Parking.API/Program.cs
@@ -1,24 +1,45 @@
+using Microsoft.OpenApi.Models;
+using Parking.Api.Middlewares;
+using Parking.Application.Extensions;
+using Parking.Infrastructure.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services
+    .AddApplication()
+    .AddInfrastructure(builder.Configuration);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Parking API", Version = "v1" });
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.Services.InitializeDatabase();
+
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Parking API v1");
+    c.RoutePrefix = string.Empty;
+});
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.UseCors("AllowFrontend");
 
 app.MapControllers();
 
